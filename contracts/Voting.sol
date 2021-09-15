a580fec3fb6ff66fd5119d25937cf66ba91dd44b
sumit-mahajan <sumitmahajan2001@gmail.com>
2021-09-15 15:19:59 +0530

    Testing with chai

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,37 +2,68 @@
 pragma solidity ^0.8.6;
 
 contract Voting {
+    // Address that creates voting program
     address public owner;
+
+    // Count of votes to A
     uint public countA;
+
+    // Count of votes to B
     uint public countB;
+
+    // Program Title
     string public title;
+
+    // Program Description
     string public description;
-    bool public isResultDeclared;
+
+    // To check if results have been announced
+    bool public isResultAnnounced;
+
+    // To store all the addresses who have already voted
     mapping(address => bool) public voters;
+
+    // Emitted when results are announced successfully
     event ResultAnnounced(uint countA, uint countB);
 
     constructor(string memory _title, string memory _description) {
+        // To check _title and _description are not empty strings
         // require(_title != "", "No title provided");
         // require(_description != "", "No description provided");
+
         owner = msg.sender;
         title = _title;
         description = _description;
     }
 
+    // Function to cast vote to either A or B
     function vote(bool isA) public {
-        require(!isResultDeclared, "Can't vote after result announcement");
-        require(!voters[msg.sender], "You have already voted");
-        voters[msg.sender] = true;
+        // Check if result already announced
+        require(!isResultAnnounced, 'Cannot vote after result announcement');
+        // Check if voter has already voted
+        require(!voters[msg.sender], 'You have already voted');
+        
+        // If true then vote for A else B
         if(isA) {
             countA++;
         } else{
             countB++;
         }
+        // Mark as voted
+        voters[msg.sender] = true;
     }
 
+    // Function to announce results which can be called by only owner
     function announceResult() public {
-        require(msg.sender == owner, "Only owner can announce result");
-        isResultDeclared = true;
+        // Check if results already announced
+        require(!isResultAnnounced, 'Result already announced');
+        // Check if caller is owner
+        require(msg.sender == owner, 'Only owner can announce result');
+
+        // Mark result as announced
+        isResultAnnounced = true;
+
+        // Emit event along with result data
         emit ResultAnnounced(countA, countB);
     }
 }
